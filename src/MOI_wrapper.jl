5d5feac9bdef229c51e399ed65da3c5228792a04
Benoît Legat <benoit.legat@gmail.com>
2019-09-27 19:49:30 +0200

    Finish MOI interface by adding support for SOC and SDP

--- src/MOI_wrapper.jl
+++ src/MOI_wrapper.jl
@@ -5,7 +5,7 @@ const MOI = MathOptInterface
 const MOIU = MOI.Utilities
 const AFFEQ = MOI.ConstraintIndex{MOI.ScalarAffineFunction{Cdouble}, MOI.EqualTo{Cdouble}}
 
-@enum VariableType FREE NNEG SOC PSD
+@enum VariableType FREE NNEG QUAD PSD
 
 struct VariableInfo
     variable_type::VariableType
@@ -18,13 +18,32 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
 
     variable_info::Vector{VariableInfo}
 
+    # PSDCone variables
+    psdc_dims::Vector{Int}
+    psdc_Cvar::Vector{Vector{Int}}
+    psdc_Cval::Vector{Vector{Float64}}
+    psdc_Avar::Vector{Vector{Int}}
+    psdc_Acon::Vector{Vector{Int}}
+    psdc_Aval::Vector{Vector{Float64}}
+
+    # QUADratic/second-order cone variables
+    quad_dims::Vector{Int}
+    quad_Cvar::Vector{Vector{Int}}
+    quad_Cval::Vector{Vector{Float64}}
+    quad_Avar::Vector{Vector{Int}}
+    quad_Acon::Vector{Vector{Int}}
+    quad_Aval::Vector{Vector{Float64}}
+
+    # NonNEGatives variables
     num_nneg::Int
+    nneg_info::Vector{Int} # Similar to `info` field of `MOI.Bridges.Variable.Map`.
     nneg_Cvar::Vector{Int}
     nneg_Cval::Vector{Float64}
     nneg_Avar::Vector{Int}
     nneg_Acon::Vector{Int}
     nneg_Aval::Vector{Float64}
 
+    # FREE variables
     num_free::Int
     free_Cvar::Vector{Int}
     free_Cval::Vector{Float64}
@@ -39,9 +58,13 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
     dual_objective_value::Float64
     free_X::Vector{Float64}
     nneg_X::Vector{Float64}
+    quad_X::Vector{Vector{Float64}}
+    psdc_X::Vector{Vector{Float64}}
     y::Vector{Float64}
     free_Z::Vector{Float64}
     nneg_Z::Vector{Float64}
+    quad_Z::Vector{Vector{Float64}}
+    psdc_Z::Vector{Vector{Float64}}
     info::Dict{String, Any}
     runhist::Dict{String, Any}
     status::Union{Nothing, Int}
@@ -52,10 +75,15 @@ mutable struct Optimizer <: MOI.AbstractOptimizer
     function Optimizer(; kwargs...)
         optimizer = new(
             Float64[], VariableInfo[],
-            0, Int[], Float64[], Int[], Int[], Float64[],
+            Int[], Vector{Int}[], Vector{Float64}[], Vector{Int}[], Vector{Int}[], Vector{Float64}[],
+            Int[], Vector{Int}[], Vector{Float64}[], Vector{Int}[], Vector{Int}[], Vector{Float64}[],
+            0, Int[], Int[], Float64[], Int[], Int[], Float64[],
             0, Int[], Float64[], Int[], Int[], Float64[],
             1, 0.0,
-            NaN, NaN, Float64[], Float64[], Float64[], Float64[], Float64[],
+            NaN, NaN,
+            Float64[], Float64[], Vector{Float64}[], Vector{Float64}[], # X
+            Float64[], # y
+            Float64[], Float64[], Vector{Float64}[], Vector{Float64}[], # Z
             Dict{String, Any}(), Dict{String, Any}(),
             nothing, NaN,
             false, Dict{Symbol, Any}())
@@ -94,6 +122,8 @@ end
 function MOI.is_empty(optimizer::Optimizer)
     return isempty(optimizer.b) &&
         isempty(optimizer.variable_info) &&
+        iszero(optimizer.psdc_dims) &&
+        iszero(optimizer.quad_dims) &&
         iszero(optimizer.num_nneg) &&
         iszero(optimizer.num_free) &&
         isone(optimizer.objective_sign) &&
@@ -105,7 +135,22 @@ function MOI.empty!(optimizer::Optimizer)
 
     empty!(optimizer.variable_info)
 
+    empty!(optimizer.psdc_dims)
+    empty!(optimizer.psdc_Cvar)
+    empty!(optimizer.psdc_Cval)
+    empty!(optimizer.psdc_Avar)
+    empty!(optimizer.psdc_Acon)
+    empty!(optimizer.psdc_Aval)
+
+    empty!(optimizer.quad_dims)
+    empty!(optimizer.quad_Cvar)
+    empty!(optimizer.quad_Cval)
+    empty!(optimizer.quad_Avar)
+    empty!(optimizer.quad_Acon)
+    empty!(optimizer.quad_Aval)
+
     optimizer.num_nneg = 0
+    empty!(optimizer.nneg_info)
     empty!(optimizer.nneg_Cvar)
     empty!(optimizer.nneg_Cval)
     empty!(optimizer.nneg_Avar)
@@ -126,9 +171,13 @@ function MOI.empty!(optimizer::Optimizer)
     optimizer.dual_objective_value = NaN
     empty!(optimizer.free_X)
     empty!(optimizer.nneg_X)
+    empty!(optimizer.quad_X)
+    empty!(optimizer.psdc_X)
     empty!(optimizer.y)
     empty!(optimizer.free_Z)
     empty!(optimizer.nneg_Z)
+    empty!(optimizer.quad_Z)
+    empty!(optimizer.psdc_Z)
     empty!(optimizer.info)
     empty!(optimizer.runhist)
     optimizer.status = nothing
@@ -138,8 +187,7 @@ end
 function MOI.supports(
     optimizer::Optimizer,
     ::Union{MOI.ObjectiveSense,
-            MOI.ObjectiveFunction{<:Union{MOI.SingleVariable,
-                                          MOI.ScalarAffineFunction{Cdouble}}}})
+            MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}})
     return true
 end
 
@@ -169,27 +217,65 @@ end
 function MOI.add_variables(optimizer::Optimizer, n::Integer)
     return [MOI.add_variable(optimizer) for i in 1:n]
 end
-function add_nonneg_variable(optimizer::Optimizer)
+function _add_nonneg_variable(optimizer::Optimizer)
     optimizer.num_nneg += 1
     push!(optimizer.variable_info, VariableInfo(NNEG, 1, optimizer.num_nneg))
     return MOI.VariableIndex(length(optimizer.variable_info))
 end
-function MOI.add_constrained_variables(optimizer::Optimizer, set::MOI.Nonnegatives)
-    ci = MOI.ConstraintIndex{MOI.VectorOfVariables, MOI.Nonnegatives}(optimizer.num_nneg + 1)
-    return [add_nonneg_variable(optimizer) for i in 1:MOI.dimension(set)], ci
+function _add_quad_variable(optimizer::Optimizer, index_in_cone)
+    push!(optimizer.variable_info, VariableInfo(QUAD, length(optimizer.quad_dims), index_in_cone))
+    return MOI.VariableIndex(length(optimizer.variable_info))
+end
+function _add_psdc_variable(optimizer::Optimizer, index_in_cone)
+    push!(optimizer.variable_info, VariableInfo(PSD, length(optimizer.psdc_dims), index_in_cone))
+    return MOI.VariableIndex(length(optimizer.variable_info))
+end
+function _add_constrained_variables(optimizer::Optimizer, set::MOI.Nonnegatives)
+    push!(optimizer.nneg_info, MOI.dimension(set))
+    for i in 2:MOI.dimension(set)
+        push!(optimizer.nneg_info, i)
+    end
+    return [_add_nonneg_variable(optimizer) for i in 1:MOI.dimension(set)]
+end
+function _add_constrained_variables(optimizer::Optimizer, set::MOI.SecondOrderCone)
+    push!(optimizer.quad_dims, MOI.dimension(set))
+    push!(optimizer.quad_Cvar, Int[])
+    push!(optimizer.quad_Cval, Float64[])
+    push!(optimizer.quad_Avar, Int[])
+    push!(optimizer.quad_Acon, Int[])
+    push!(optimizer.quad_Aval, Float64[])
+    return [_add_quad_variable(optimizer, i) for i in 1:MOI.dimension(set)]
+end
+function _add_constrained_variables(optimizer::Optimizer, set::MOI.PositiveSemidefiniteConeTriangle)
+    push!(optimizer.psdc_dims, MOI.side_dimension(set))
+    push!(optimizer.psdc_Cvar, Int[])
+    push!(optimizer.psdc_Cval, Float64[])
+    push!(optimizer.psdc_Avar, Int[])
+    push!(optimizer.psdc_Acon, Int[])
+    push!(optimizer.psdc_Aval, Float64[])
+    return [_add_psdc_variable(optimizer, i) for i in 1:MOI.dimension(set)]
+end
+function MOI.add_constrained_variables(optimizer::Optimizer, set::SupportedSets)
+    vis = _add_constrained_variables(optimizer, set)
+    ci = MOI.ConstraintIndex{MOI.VectorOfVariables, typeof(set)}(first(vis).value)
+    return vis, ci
 end
 
 # Objective
 function MOI.set(optimizer::Optimizer, ::MOI.ObjectiveSense, sense::MOI.OptimizationSense)
-    # To be sure that it is done before load(optimizer, ::ObjectiveFunction, ...), we do it in allocate
     sign = sense == MOI.MAX_SENSE ? -1 : 1
     if sign != optimizer.objective_sign
         rmul!(optimizer.free_Cval, -1)
         rmul!(optimizer.nneg_Cval, -1)
+        for i in eachindex(optimizer.quad_dims)
+            rmul!(optimizer.quad_Cval[i], -1)
+        end
+        for i in eachindex(optimizer.psdc_dims)
+            rmul!(optimizer.psdc_Cval[i], -1)
+        end
     end
     optimizer.objective_sign = sign
 end
-MOI.supports(::Optimizer, ::MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}) = true
 function MOI.set(optimizer::Optimizer, ::MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}},
                  func::MOI.ScalarAffineFunction{Float64})
     optimizer.objective_constant = MOI.constant(func)
@@ -197,6 +283,14 @@ function MOI.set(optimizer::Optimizer, ::MOI.ObjectiveFunction{MOI.ScalarAffineF
     empty!(optimizer.free_Cval)
     empty!(optimizer.nneg_Cvar)
     empty!(optimizer.nneg_Cval)
+    for i in eachindex(optimizer.quad_dims)
+        empty!(optimizer.quad_Cvar[i])
+        empty!(optimizer.quad_Cval[i])
+    end
+    for i in eachindex(optimizer.psdc_dims)
+        empty!(optimizer.psdc_Cvar[i])
+        empty!(optimizer.psdc_Cval[i])
+    end
     for term in func.terms
         info = optimizer.variable_info[term.variable_index.value]
         if info.variable_type == FREE
@@ -205,17 +299,24 @@ function MOI.set(optimizer::Optimizer, ::MOI.ObjectiveFunction{MOI.ScalarAffineF
         elseif info.variable_type == NNEG
             push!(optimizer.nneg_Cvar, info.index_in_cone)
             push!(optimizer.nneg_Cval, optimizer.objective_sign * term.coefficient)
+        elseif info.variable_type == QUAD
+            push!(optimizer.quad_Cvar[info.cone_index], info.index_in_cone)
+            push!(optimizer.quad_Cval[info.cone_index], optimizer.objective_sign * term.coefficient)
         else
-            @assert false
+            @assert info.variable_type == PSD
+            push!(optimizer.psdc_Cvar[info.cone_index], info.index_in_cone)
+            push!(optimizer.psdc_Cval[info.cone_index], optimizer.objective_sign * term.coefficient)
         end
     end
 end
-function MOI.set(optimizer::Optimizer, ::MOI.ObjectiveFunction, f::MOI.SingleVariable)
-    MOI.set(optimizer, MOI.ObjectiveFunction{MOI.ScalarAffineFunction{Float64}}(),
-            convert(MOI.ScalarAffineFunction{Float64}, f))
-end
 
 # Constraints
+function is_diagonal_index(k)
+    # See https://www.juliaopt.org/MathOptInterface.jl/v0.9.3/apireference/#MathOptInterface.AbstractSymmetricMatrixSetTriangle
+    i = div(1 + isqrt(8k - 7), 2)
+    j = k - div((i - 1) * i, 2)
+    return i == j
+end
 function MOI.add_constraint(optimizer::Optimizer, func::MOI.ScalarAffineFunction{Float64}, set::MOI.EqualTo{Float64})
     if !iszero(MOI.constant(func))
         # We use the fact that the initial function constant was zero to
@@ -235,11 +336,34 @@ function MOI.add_constraint(optimizer::Optimizer, func::MOI.ScalarAffineFunction
             push!(optimizer.nneg_Avar, info.index_in_cone)
             push!(optimizer.nneg_Acon, con)
             push!(optimizer.nneg_Aval, term.coefficient)
+        elseif info.variable_type == QUAD
+            push!(optimizer.quad_Avar[info.cone_index], info.index_in_cone)
+            push!(optimizer.quad_Acon[info.cone_index], con)
+            push!(optimizer.quad_Aval[info.cone_index], term.coefficient)
         else
-            @assert false
+            @assert info.variable_type == PSD
+            push!(optimizer.psdc_Avar[info.cone_index], info.index_in_cone)
+            push!(optimizer.psdc_Acon[info.cone_index], con)
+            coef = is_diagonal_index(info.index_in_cone) ? term.coefficient : term.coefficient / √2
+            push!(optimizer.psdc_Aval[info.cone_index], coef)
         end
     end
     return AFFEQ(con)
+end
+
+function symvec(Q::Matrix)
+    n = LinearAlgebra.checksquare(Q)
+    vec_dim = MOI.dimension(MOI.PositiveSemidefiniteConeTriangle(n))
+    q = Vector{eltype(Q)}(undef, vec_dim)
+    k = 0
+    for j in 1:n
+        for i in 1:j
+            k += 1
+            q[k] = Q[i, j]
+        end
+    end
+    @assert k == length(q)
+    return q
 end
 
 function MOI.optimize!(optimizer::Optimizer)
@@ -249,22 +373,54 @@ function MOI.optimize!(optimizer::Optimizer)
         options[:printlevel] = 0
     end
 
-    blk = Matrix{Any}(undef, 0, 2)
-    blkt_vec = Any[]
+    blk1 = Any[]
+    blk2 = Any[]
     m = length(optimizer.b)
     At = SparseArrays.SparseMatrixCSC{Float64,Int}[]
     # FIXME I get a strange failure with sparse vectors, need to investigate
-    C = Vector{Float64}[]
+    C = Union{Matrix{Float64}, Vector{Float64}}[]
+    if !isempty(optimizer.psdc_dims)
+        for (i, dim) in enumerate(optimizer.psdc_dims)
+            vec_dim = MOI.dimension(MOI.PositiveSemidefiniteConeTriangle(dim))
+            push!(At, sparse(optimizer.psdc_Avar[i], optimizer.psdc_Acon[i], optimizer.psdc_Aval[i], vec_dim, m))
+            c = Vector(sparsevec(optimizer.psdc_Cvar[i], optimizer.psdc_Cval[i], vec_dim))
+            Ci = zeros(dim, dim)
+            k = 0
+            for col in 1:dim
+                for row in 1:(col - 1)
+                    k += 1
+                    Ci[row, col] = c[k] / 2
+                    Ci[col, row] = c[k] / 2
+                end
+                k += 1
+                Ci[col, col] = c[k]
+            end
+            push!(C, Ci)
+            push!(blk1, "s")
+            push!(blk2, Float64(dim))
+        end
+    end
+    if !isempty(optimizer.quad_dims)
+        for (i, dim) in enumerate(optimizer.quad_dims)
+            push!(At, sparse(optimizer.quad_Avar[i], optimizer.quad_Acon[i], optimizer.quad_Aval[i], dim, m))
+            push!(C, Vector(sparsevec(optimizer.quad_Cvar[i], optimizer.quad_Cval[i], dim)))
+            push!(blk1, "q")
+            push!(blk2, Float64(dim))
+        end
+    end
     if !iszero(optimizer.num_nneg)
         push!(At, sparse(optimizer.nneg_Avar, optimizer.nneg_Acon, optimizer.nneg_Aval, optimizer.num_nneg, m))
         push!(C, Vector(sparsevec(optimizer.nneg_Cvar, optimizer.nneg_Cval, optimizer.num_nneg)))
-        blk = vcat(blk, ["l" optimizer.num_nneg])
+        push!(blk1, "l")
+        push!(blk2, Float64(optimizer.num_nneg))
     end
     if !iszero(optimizer.num_free)
         push!(At, sparse(optimizer.free_Avar, optimizer.free_Acon, optimizer.free_Aval, optimizer.num_free, m))
         push!(C, Vector(sparsevec(optimizer.free_Cvar, optimizer.free_Cval, optimizer.num_free)))
-        blk = vcat(blk, ["u" optimizer.num_free])
+        push!(blk1, "u")
+        push!(blk2, Float64(optimizer.num_free))
     end
+    blk = [blk1 blk2]
 
     options = optimizer.options
     if optimizer.silent
@@ -277,6 +433,12 @@ function MOI.optimize!(optimizer::Optimizer)
 
     optimizer.primal_objective_value, optimizer.dual_objective_value = obj
     k = 0
+    optimizer.psdc_X = symvec.(X[k .+ eachindex(optimizer.psdc_dims)])
+    optimizer.psdc_Z = symvec.(Z[k .+ eachindex(optimizer.psdc_dims)])
+    k += length(optimizer.psdc_dims)
+    optimizer.quad_X = X[k .+ eachindex(optimizer.quad_dims)]
+    optimizer.quad_Z = Z[k .+ eachindex(optimizer.quad_dims)]
+    k += length(optimizer.quad_dims)
     if iszero(optimizer.num_nneg)
         empty!(optimizer.nneg_X)
         empty!(optimizer.nneg_Z)
@@ -373,7 +535,6 @@ function MOI.get(m::Optimizer, ::MOI.ObjectiveValue)
     return m.objective_sign * m.primal_objective_value + m.objective_constant
 end
 function MOI.get(m::Optimizer, ::MOI.DualObjectiveValue)
-    # FIXME this is the primal obj
     return m.objective_sign * m.dual_objective_value + m.objective_constant
 end
 
@@ -383,8 +544,11 @@ function MOI.get(optimizer::Optimizer, ::MOI.VariablePrimal, vi::MOI.VariableInd
         return optimizer.free_X[info.index_in_cone]
     elseif info.variable_type == NNEG
         return optimizer.nneg_X[info.index_in_cone]
+    elseif info.variable_type == QUAD
+        return optimizer.quad_X[info.cone_index][info.index_in_cone]
     else
-        @assert false
+        @assert info.variable_type == PSD
+        return optimizer.psdc_X[info.cone_index][info.index_in_cone]
     end
 end
 
@@ -392,12 +556,15 @@ function MOI.get(optimizer::Optimizer, ::MOI.ConstraintPrimal,
                  ci::MOI.ConstraintIndex{MOI.VectorOfVariables, S}) where S<:SupportedSets
     info = optimizer.variable_info[ci.value]
     if info.variable_type == FREE
-        @assert false
+        error("No constraint primal for free variables.")
     elseif info.variable_type == NNEG
-        # TODO need to figure out dim
-        return [optimizer.nneg_X[info.index_in_cone]]
+        dim = optimizer.nneg_info[info.index_in_cone]
+        return optimizer.nneg_X[(info.index_in_cone - 1) .+ (1:dim)]
+    elseif info.variable_type == QUAD
+        return optimizer.quad_X[info.cone_index]
     else
-        @assert false
+        @assert info.variable_type == PSD
+        return optimizer.psdc_X[info.cone_index]
     end
 end
 function MOI.get(optimizer::Optimizer, ::MOI.ConstraintPrimal, ci::AFFEQ)
@@ -408,12 +575,15 @@ function MOI.get(optimizer::Optimizer, ::MOI.ConstraintDual,
                  ci::MOI.ConstraintIndex{MOI.VectorOfVariables, S}) where S<:SupportedSets
     info = optimizer.variable_info[ci.value]
     if info.variable_type == FREE
-        @assert false
+        error("No constraint dual for free variables.")
     elseif info.variable_type == NNEG
-        # TODO need to figure out dim
-        return [optimizer.nneg_Z[info.index_in_cone]]
+        dim = optimizer.nneg_info[info.index_in_cone]
+        return optimizer.nneg_Z[(info.index_in_cone - 1) .+ (1:dim)]
+    elseif info.variable_type == QUAD
+        return optimizer.quad_Z[info.cone_index]
     else
-        @assert false
+        @assert info.variable_type == PSD
+        return optimizer.psdc_Z[info.cone_index]
     end
 end
 function MOI.get(optimizer::Optimizer, ::MOI.ConstraintDual, ci::AFFEQ)
--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -7,23 +7,23 @@ const MOIU = MOI.Utilities
 const MOIB = MOI.Bridges
 
 import SDPT3
-const optimizer = SDPT3.Optimizer()
-MOI.set(optimizer, MOI.Silent(), true)
+const OPTIMIZER = SDPT3.Optimizer()
+MOI.set(OPTIMIZER, MOI.Silent(), true)
 
 @testset "SolverName" begin
-    @test MOI.get(optimizer, MOI.SolverName()) == "SDPT3"
+    @test MOI.get(OPTIMIZER, MOI.SolverName()) == "SDPT3"
 end
 
 @testset "supports_default_copy_to" begin
-    @test MOIU.supports_default_copy_to(optimizer, false)
-    @test !MOIU.supports_default_copy_to(optimizer, true)
+    @test MOIU.supports_default_copy_to(OPTIMIZER, false)
+    @test !MOIU.supports_default_copy_to(OPTIMIZER, true)
 end
 
 # UniversalFallback is needed for starting values, even if they are ignored by SDPT3
-const cache = MOIU.UniversalFallback(MOIU.Model{Float64}())
-const cached = MOIU.CachingOptimizer(cache, optimizer)
-const bridged = MOIB.full_bridge_optimizer(cached, Float64)
-const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
+const CACHE = MOIU.UniversalFallback(MOIU.Model{Float64}())
+const CACHED = MOIU.CachingOptimizer(CACHE, OPTIMIZER)
+const BRIDGED = MOIB.full_bridge_optimizer(CACHED, Float64)
+const CONFIG = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 
 @testset "Options" begin
     param = MOI.RawParameter(:bad_option)
@@ -32,7 +32,7 @@ const config = MOIT.TestConfig(atol=1e-4, rtol=1e-4)
 end
 
 @testset "Unit" begin
-    MOIT.unittest(bridged, config, [
+    MOIT.unittest(BRIDGED, CONFIG, [
         # Get `termcode` -1, i.e. "relative gap < infeasibility".
         "solve_blank_obj",
         # Get `termcode` 3, i.e. "norm(X) or norm(Z) diverging".
@@ -50,13 +50,36 @@ end
         "solve_zero_one_with_bounds_3"])
 end
 @testset "Continuous Linear" begin
-    # See explanation in `MOI/test/Bridges/lazy_bridge_optimizer.jl`.
+    # See explanation in `MOI/test/Bridges/lazy_bridge_OPTIMIZER.jl`.
     # This is to avoid `Variable.VectorizeBridge` which does not support
     # `ConstraintSet` modification.
-    MOIB.remove_bridge(bridged, MOIB.Constraint.ScalarSlackBridge{Float64})
-    MOIT.contlineartest(bridged, config, String[
+    MOIB.remove_bridge(BRIDGED, MOIB.Constraint.ScalarSlackBridge{Float64})
+    MOIT.contlineartest(BRIDGED, CONFIG, String[
         # Throws error: total dimension of C should be > length(b)
         "linear15",
         "partial_start"
+    ])
+end
+@testset "Continuous Quadratic" begin
+    MOIT.contquadratictest(BRIDGED, CONFIG, [
+        # FIXME CachingOptimizer does not implement deletion of vector of variables and uses the fallback. Need upstream MOI fix.
+        "qp2", "qp3",
+        # Non-convex
+        "ncqcp",
+        # Quadratic function not strictly convex
+        "socp"])
+end
+@testset "Continuous Conic" begin
+    MOIT.contconictest(BRIDGED, CONFIG, [
+        # `MOI.OTHER_ERROR`
+        "lin2f",
+        # `MOI.NUMERICAL_ERROR`
+        "lin4",
+        # `MOI.NUMERICAL_ERROR`: should be fixed by a RSOC->SOC Variable bridge
+        "rotatedsoc2",
+        # `ExponentialCone` and `PowerCone` not supported.
+        "exp", "dualexp", "pow", "dualpow", "logdet",
+        # `RootDetConeSquare` -> `RootDetConeTriangle` bridge missing.
+        "rootdets"
     ])
 end
